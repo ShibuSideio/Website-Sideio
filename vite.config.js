df58c427c26358640ca1e46727492ddd28d712d9
ShibuSideio <shibu.thomas@sideio.com>
2025-12-24 14:10:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
-    host: '0.0.0.0',
-    port: 8080
+    host: '0.0.0.0', // This is the secret to fixing the 8080 error
+    port: 8080,
+    strictPort: true
   },
   preview: {
     host: '0.0.0.0',
-    port: 8080
+    port: 8080,
+    strictPort: true
   },
   build: {
     outDir: 'dist'
